9948ac6f2813019da75e1fc1a87a9605a77e9ad9
Dmitry Volkov <99538385+Avdors@users.noreply.github.com>
2024-08-04 16:15:52 +0200

    main activity

--- app/src/main/java/com/example/projdiffutil/MainActivity.kt
+++ app/src/main/java/com/example/projdiffutil/MainActivity.kt
@@ -2,19 +2,33 @@ package com.example.projdiffutil
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.projdiffutil.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
+    private val adapter = ItemAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+
+        viewModel.items.observe(this, {items ->
+            adapter.submitList((items))
+        })
+
+        binding.btUpdate.setOnClickListener{
+            viewModel.updateItems()
         }
+
     }
 }
